f7f6c500ff6261adad369fbb3564b7cd46fd5edc
David Alvarez <48391594+aalvag@users.noreply.github.com>
2022-04-14 17:01:36 -0300

    Update clase2.js
    Hola, te dejado comentado las modificaciones que tienes que hacer.

--- clase2.js
+++ clase2.js
@@ -5,15 +5,18 @@ class usuario {
         this.mascotas = [];
         this.libros = [];
     }
+    // debe recibir 4 parametros y se asigna al this.variable
 
 
     getFullName() {
         return console.log("Su nombre es:" + this.nombre + "" + this.apellido);
     }
-
+// debe usar template string
+    
     addMascotas(mascotas) {
         return this.mascotas.push(mascotas);
     }
+    // esta funcion no deberia retornar nada
 
     countMascotas() {
         return this.mascotas.length;
@@ -22,18 +25,23 @@ class usuario {
     addBook(nombre, autor) {
         return this.libros.push(`{nombre: ${nombre}, autor: ${autor}`);
     }
-
+  // no debe retornar nada  
+    
     getBookNames() {
         this.libros.map((libro) => {
             return libro.nombre;
         });
     }
+    
 }
 
 let referencia = new usuario("Lucas", "Poma");
 let referencia2 = new usuario("Mariana", "Gomez");
 let referencia3 = new usuario();
 let referencia4 = new usuario();
+
+// debes crear un objeto llamdo usuario con valores
+// const usuario = new usuario({...crear el objeto})
 
 referencia.getFullName();
 referencia2.getFullName();
@@ -45,3 +53,5 @@ console.log(`La cantidad de mascotas es de ${referencia3.countMascotas()} `);
 referencia4.addBook("La Herencia", "John Grisham");
 
 console.log(`Libro: ${referencia4.getBookNames()}`);
+
+// no hagas console.log simplemente ejecuta los metodos
